5e1085f4b56c2b5dd8954957a037d22736a66730
Himanshu Kumar Sahu <himanshukumarsahu@LEM20164.local>
2025-07-13 00:55:15 +0530

    remove element

--- two-pointer/easy/leet_code_27_remove_element.js
+++ two-pointer/easy/leet_code_27_remove_element.js
@@ -2,12 +2,13 @@
 //https://leetcode.com/problems/remove-element/
 
 //Solution 1
-// Idea: Use filter method to remove all elements equal to val
+// Idea: Use filter method to remove all elements equal to val 
 // TC O(n)
+// SC O(n) for the new array created by filter
 
 //Solution 2
 // TC O(n)
-// SC O(1)
+// SC O(1) In place
 //Idea; element should be removed if it is equal to val so we maintain a pointer to track the index of current correct index;
 var removeElement = function(nums, val) {
     let index = 0; 
